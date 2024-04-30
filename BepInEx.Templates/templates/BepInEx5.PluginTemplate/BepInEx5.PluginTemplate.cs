808403eeb6b7271b80a42928de2e26bb53bc45ec
Mangochicken <97166753+Mangochicken13@users.noreply.github.com>
2024-04-30 09:19:41 +0800

    Update BepInEx5.PluginTemplate.cs

--- BepInEx.Templates/templates/BepInEx5.PluginTemplate/BepInEx5.PluginTemplate.cs
+++ BepInEx.Templates/templates/BepInEx5.PluginTemplate/BepInEx5.PluginTemplate.cs
@@ -3,17 +3,33 @@ using BepInEx.Logging;
 
 namespace BepInEx5.PluginTemplate;
 
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(ModID, ModName, ModVersion)]
 public class BepInEx5.PluginTemplate : BaseUnityPlugin
 {
+    public const string ModID = "com.mangochicken.BepInEx5.PluginTemplate";
+    public const string ModName = "BepInEx5.PluginTemplate";
+    public const string ModVersion = "{version}";
+
     internal static ManualLogSource Log;
+
+    public BepInEx5.PluginTemplate Instance { get; private set; }
         
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
         Log = Logger;
 
         // Plugin startup logic
-        
-        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Log.LogInfo($"Plugin {ModID} is loaded!");
     }
 }
